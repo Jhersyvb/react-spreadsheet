99c8c8498a551f91c272ecba9292f3e32fbf8722
Jhersy <jhersyvb@gmail.com>
2019-10-07 17:56:11 -0500

    2 Let's start

---
+++ src/components/Table.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Row from './Row'
+
+class Table extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: {}
+    }
+  }
+
+  handleChangeCell = ({ x, y }, value) => {
+    const modifiedData = Object.assign({}, this.state.data)
+    if (!modifiedData[y]) modifiedData[y] = {}
+    modifiedData[y][x] = value
+    this.setState({ data: modifiedData })
+  }
+
+  updateCells = () => {
+    this.forceUpdate()
+  }
+
+  render() {
+    const rows = []
+
+    for (let y = 0; y < this.props.y; y++) {
+      const rowData = this.state.data[y] || {}
+      rows.push(
+        <Row
+          handleChangeCell={this.handleChangeCell}
+          updateCells={this.updateCells}
+          key={y}
+          y={y}
+          x={this.props.x + 1}
+          rowData={rowData}
+        />
+      )
+    }
+
+    return <div>{rows}</div>
+  }
+}
+
+export default Table
